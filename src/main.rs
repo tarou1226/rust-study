87cd2ee936e22fe21b646f47d7709cda4a89a380
taro <teroro1226@outlook.jp>
2022-09-26 07:42:51 +0000

    exchange guess fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io; //pythonでいうところのimport
+
 fn main() {
-    println!("Hello, cargo!");
+    println!("数字当てゲーム！");
+
+    println!("数字を入力してください。");
+    // mut→可変(変数)　何も無し→不可変(定数)
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("読み込みに失敗しました");
+    // {}はformat文
+    println!("あなたの数字: {}", guess);
 }
